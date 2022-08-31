d5f700809fcd8924425ea492f7db8712f5f0ed31
Ryan Murphy <rymurphy12@gmail.com>
2022-08-31 10:01:21 -0500

    first commit

---
+++ SalesRepTracking.UI/Models/PersonalInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesRepTracking.UI.Models;
+
+public class PersonalInfo
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string AddressOne { get; set; }
+    public string AddressTwo { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public string Phone { get; set; }
+    public string email { get; set; }
+}
---
+++ SalesRepTracking.UI/Pages/AddRep.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using SalesRepTracking.UI.Models;
+
+namespace SalesRepTracking.UI.Pages;
+
+public partial class AddRep
+{
+    public PersonalInfo PersonalInfo { get; set; } = new();
+    
+    [Inject]
+    public NavigationManager NavManager { get; set; }
+    public void SubmitData()
+    {
+        // Do things
+        NavManager.NavigateTo("/success");
+    }
+}
